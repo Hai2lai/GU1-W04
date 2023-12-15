099402cb5f6f71f022b08ce1b7ed39ff1c9b8483
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-15 15:16:35 +0700

    tinh_tong_cac_phan_tu_cua_mang

---
+++ tinh_tong_cac_phan_tu_cua_mang/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = new int[5];
+        int tong=0;
+        Console.WriteLine("ban muon tinh tong cua 5 so nao");
+        for(int i=0;i<5;i++)
+        {
+            array[i]=int.Parse(Console.ReadLine());
+        }
+        for(int i=0;i<5;i++)
+        {   
+            tong += array[i];
+        }
+        Console.WriteLine(tong);
+}
+}
