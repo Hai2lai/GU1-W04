d2008ceae0e5072cf05d0af75258a1d76c59eecc
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-19 12:06:40 +0700

    tim_kiem_phan_tu_trong_mang

---
+++ tim_kiem_phan_tu_trong_mang/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] array = {"hung","huyen","hoang","hai"};
+        string ten;
+        Console.WriteLine("nhap ten ban muon tim : ");
+        ten = Console.ReadLine();
+        bool kiemtra = false;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i].Equals(ten))
+            {
+                Console.WriteLine($"co ten trong danh sach va o vi tri so {i+1}");
+                kiemtra = true;
+                break;
+            }
+        }
+        if(kiemtra == false)
+        {
+            Console.WriteLine("khong co ten trong danh sach");
+        }
+
+    }
+}
--- tinh_tong_cac_phan_tu_cua_mang/Program.cs
+++ tinh_tong_cac_phan_tu_cua_mang/Program.cs
@@ -5,7 +5,7 @@
         int[] array = new int[5];
         int tong=0;
         Console.WriteLine("ban muon tinh tong cua 5 so nao");
-        for(int i=0;i<5;i++)
+        for(int i=0;i<array.Length;i++)
         {
             array[i]=int.Parse(Console.ReadLine());
         }
