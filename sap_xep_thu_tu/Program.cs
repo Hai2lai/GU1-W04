ca37034e9d84a73f9f264a620d56e12e33af0d06
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-20 13:12:33 +0700

    sap_thu_tu_and_tim_gia_tri_lon_nhat

---
+++ sap_xep_thu_tu/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("can bao nhieu so : ");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        for(int i=0;i<n;i++)
+        {   
+            Console.Write($"array[{i}] ");
+            array[i]= int.Parse(Console.ReadLine());
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(array[j]<array[j+1])
+                {
+                    int tam;
+                    tam = array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=tam;
+                }
+            }
+        }
+        Console.WriteLine("vay sau khi sap sep la :");
+        for(int i=0;i<n;i++)
+        {
+            Console.Write(" "+array[i]);
+        }
+    }
+}
+
---
+++ tim_gia_tri_lon_nhat/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int n=123312;
+        while(n>20)
+        {
+            Console.WriteLine("nhap so luong : ");
+            n = int.Parse(Console.ReadLine());
+            if(n>20)
+            {
+                Console.WriteLine("gia tri qua muc quy dinh! vui long nhap lai");
+            }
+        }
+        int[] a = new int[n];
+        int max =0;
+        Console.WriteLine("nhap gia tri cua mang : ");
+        for(int i=0;i<n;i++)
+        {
+            Console.Write($"gia tri [{i}] = ");
+            a[i]=int.Parse(Console.ReadLine());
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                max = a[i];
+            }
+        }
+        Console.WriteLine("vay gia tri lon nhat la : "+max);
+    }
+}
