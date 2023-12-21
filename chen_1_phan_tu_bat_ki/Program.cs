052312424f1cd34e10083b146d483ce2e8295515
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-21 11:40:08 +0700

    .

---
+++ chen_1_phan_tu_bat_ki/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        Console.Write("trong mang co bao nhieu phan tu : ");
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            Console.Write($"a[{i}]");
+            a[i]=int.Parse(Console.ReadLine());
+        }
+        for(int i=0;i<n;i++)
+        {
+            Console.Write($"{a[i]} ");
+        }
+        Console.WriteLine("ban muon chen so bao nhieu");
+        int c=int.Parse(Console.ReadLine());
+        Console.WriteLine($"ban muon chen so {c} vao vi tri nao ?");
+        int vtr = int.Parse(Console.ReadLine());
+        n++;
+        Array.Resize(ref a,n);
+        for(int i=n-1;i>vtr;i--)
+        {
+            a[i]=a[i-1];
+        }
+        a[vtr]=c;
+        for(int i=0;i<n;i++)
+        {
+            Console.Write($"{a[i]} ");
+        }
+    }
+}
