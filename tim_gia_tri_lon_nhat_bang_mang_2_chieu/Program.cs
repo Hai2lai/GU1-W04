df452fc4b72ca505a8a7df47b805852b49671025
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-22 21:18:11 +0700

    .

---
+++ tim_gia_tri_lon_nhat_bang_mang_2_chieu/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+internal class Progtam
+{
+    public static void Main(string[] args)
+    {
+        /*
+        int [,] array = new int[4,5]; //cách 1 
+        int[,] aray = { {1,2,3},
+                        {4,5,6},
+                        {3,4,5},
+                      };
+        //leangth
+        //hàng getlength(0),cột getlength(1);
+        Console.WriteLine(aray.GetLength(0));
+        Console.WriteLine(aray.GetLength(1));
+        Console.WriteLine(array.GetLength(0));
+        Console.WriteLine(array.GetLength(1));
+        Console.WriteLine(aray.Length);
+        */
+        int[,] aray = { {1,2,3},
+                        {4,5,6},
+                        {3,4,5},
+                      };
+        int max = aray[0,0];
+        for(int i=0;i<aray.GetLength(0);i++)
+        {
+            for(int j=0;j<aray.GetLength(1);j++)
+            {
+                if(aray[i,j]>max)
+                {
+                   max = aray[i,j];
+                }
+            }
+        }
+        Console.WriteLine("gia tri lon nhat trong mang 2 chieu la "+max);
+    }
+}
---
+++ tinh_tong_duong_cheo_trong _mang_2_chieu/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        Console.WriteLine("nhap gia tri cua hang");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("nhap gia tri cua cot");
+        int m = int.Parse(Console.ReadLine());
+        int [,] array = new int[n,m];
+        Console.WriteLine("nhap cac phan tu trong mang");
+        for(int i=0;i<array.GetLength(0);i++)
+        {
+            for(int j=0;j<array.GetLength(1);j++)
+            {
+                array[i,j]= int.Parse(Console.ReadLine());
+            }
+        }
+        //in cac phan tu trong mang
+        for(int i=0;i<n;i++)
+        {
+            Console.WriteLine();
+            for(int j=0;j<m;j++)
+            {
+                Console.Write(array[i,j]+" ");
+            }
+        }
+        int tong=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==j)
+                {
+                    tong += array[i,j];
+                }
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("vay gia tri duong cheo la : "+tong);
+    }
+}
